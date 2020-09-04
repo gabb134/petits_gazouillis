32900affeacbbced9a6e9e1cd5490c6fb15dab95
gabb134 <cg.marrero@cgodin.qc.ca>
2020-09-03 23:34:03 -0400

    modif gab, labo 2 complete avec erreur dans etablirsession.html, voir avec le prof

--- app/formulaires.py
+++ app/formulaires.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class FormulaireEtablirSession(FlaskForm):
-    nom = StringField('Nom', validators=[DataRequired()])
-    mot_de_passe = PasswordField('Mot de passe', validators=[DataRequired()])
+    nom = StringField('Nom', validators=[DataRequired(message="Nom est un champ obligatoire")])
+    mot_de_passe = PasswordField('Mot de passe', validators=[DataRequired(message="Mot de passe est un champ obligatoire")])
     se_souvenir_de_moi = BooleanField('Se souvenir de moi')
     soumettre = SubmitField('Etablir une session')
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash,redirect,url_for
 from app import app
 from app.formulaires import FormulaireEtablirSession
 
@@ -19,7 +19,10 @@ def index():
     return render_template('index.html', titrex='Accueil', utilisateur=abc,publications=publications) 
 
 
-@app.route('/etablir_session')
+@app.route('/etablir_session',methods=['GET','POST'])
 def etablir_session():
     formulaire = FormulaireEtablirSession()
+    if formulaire.validate_on_submit():
+        flash('Établir une session par utilisateur {}, se_souvenir_de_moi ={}'.format(formulaire.nom.data, formulaire.se_souvenir_de_moi.data))
+        return redirect( url_for('index') )
     return render_template('etablir_session.html', titre='Établir une session', formulaire=formulaire)  
