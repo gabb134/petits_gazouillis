e841e4cc2d741de0d0834fbdb25fc32356ba23f9
gabb134 <cg.marrero@cgodin.qc.ca>
2020-09-13 22:30:29 -0400

    modif gab labo 4 en cours

--- petits_gazouillis.py
+++ petits_gazouillis.py
@@ -1,5 +1,15 @@
 from app import app, db
 from app.models import Utilisateur, Publication
+
+
+
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'U': Utilisateur, 'P': Publication}
+
+print('Demarrage petits gazouillis')    
+
+@app.before_first_request
+def initialisation():
+    print('initialisation')
+    
