32ba1c15282f1371986a5e519968247b8b428edd
gabb134 <cg.marrero@cgodin.qc.ca>
2020-09-17 22:07:42 -0400

    modif gab labo 5 termnine

--- app/routes.py
+++ app/routes.py
@@ -30,7 +30,7 @@ def enregistrer():
     if formulaire.validate_on_submit():
         utilisateur = Utilisateur(nom=formulaire.nom.data, courriel = formulaire.courriel.data)
         utilisateur.enregistrer_mot_de_passe(formulaire.mot_de_passe.data)
-      '''  fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf',15)
+       fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf',15)
         image = Image.new('RGB',(128,128), color = 'black')
         for i in range(20):
             x = random.randint(0,128)
@@ -48,7 +48,7 @@ def enregistrer():
         # "data:image/jpg;base64,"
         image_base64 = base64.b64encode(tampon.getvalue()).decode("utf-8")
         utilisateur.avatar = "data:image/jpg;base64," + image_base64
-        print("data:image/jpg;base64," + image_base64) '''
+        print("data:image/jpg;base64," + image_base64)
 
         db.session.add(utilisateur)
         db.session.commit()
