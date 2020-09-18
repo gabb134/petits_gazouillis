1fc7948963a8164c0d655c64260dde7312ab8dcb
gabb134 <cg.marrero@cgodin.qc.ca>
2020-09-17 22:07:08 -0400

    modif gab labo 5 termnine

--- app/formulaires.py
+++ app/formulaires.py
@@ -1,9 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError ,Email, EqualTo
+from app.models import Utilisateur
 
 class FormulaireEtablirSession(FlaskForm):
     nom = StringField('Nom', validators=[DataRequired(message="Nom est un champ obligatoire")])
     mot_de_passe = PasswordField('Mot de passe', validators=[DataRequired(message="Mot de passe est un champ obligatoire")])
     se_souvenir_de_moi = BooleanField('Se souvenir de moi')
     soumettre = SubmitField('Etablir une session')
+
+class FormulaireEnregistrement(FlaskForm):
+    nom = StringField('Nom', validators=[DataRequired()])
+    courriel = StringField('Courriel', validators= [DataRequired(), Email()])
+    mot_de_passe = PasswordField('Mot de passe', validators=[DataRequired()])
+    mot_de_passe2 = PasswordField('Mot de passe (entrer à nouveau)', validators=[DataRequired(), EqualTo('mot_de_passe')])
+    soumettre = SubmitField('Enregistrer')
+
+    def validate_nom(self,nom):
+        utilisateur = Utilisateur.query.filter_by(nom=nom.data).first()
+        if utilisateur is not None:
+            raise ValidationError('Ce nom existe déjà.')
+
+    def validate_courriel(self, courriel):
+        utilisateur = Utilisateur.query.filter_by(courriel=courriel.data).first()
+        if utilisateur is not None:
+            raise ValidationError('Ce courriel existe déjà...')
--- app/routes.py
+++ app/routes.py
@@ -6,9 +6,9 @@ from flask_login import current_user, login_user, logout_user
 from flask import request
 from werkzeug.urls import url_parse
 from flask_login import login_required
-#from app.formulaires import FormulaireEnregistrement
+from app.formulaires import FormulaireEnregistrement
 from app import db
-#from PIL import Image, ImageDraw, ImageFront
+from PIL import Image, ImageDraw, ImageFont
 import random
 import base64 
 from io import BytesIO
@@ -21,6 +21,42 @@ def index():
     publication = current_user.publication.all()
     #utilisateur = Utilisateur.query.all()
     return render_template('index.html', titrex='Accueil', utilisateur=utilisateur,publication=publication) 
+
+@app.route('/enregistrer',methods=['GET','POST'])
+def enregistrer():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    formulaire = FormulaireEnregistrement()
+    if formulaire.validate_on_submit():
+        utilisateur = Utilisateur(nom=formulaire.nom.data, courriel = formulaire.courriel.data)
+        utilisateur.enregistrer_mot_de_passe(formulaire.mot_de_passe.data)
+      '''  fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf',15)
+        image = Image.new('RGB',(128,128), color = 'black')
+        for i in range(20):
+            x = random.randint(0,128)
+            y = random.randint(0,128)
+            r = random.randint(0,255)
+            g = random.randint(0,255)
+            b = random.randint(0,255)
+            h = random.randint(10,20)
+            fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf',h)
+            d = ImageDraw.Draw(image)
+            d.text((x,y), utilisateur.nom, font = fnt, fill = (r,g,g))
+
+        tampon = BytesIO()
+        image.save(tampon,format="JPEG")
+        # "data:image/jpg;base64,"
+        image_base64 = base64.b64encode(tampon.getvalue()).decode("utf-8")
+        utilisateur.avatar = "data:image/jpg;base64," + image_base64
+        print("data:image/jpg;base64," + image_base64) '''
+
+        db.session.add(utilisateur)
+        db.session.commit()
+        flash('Félicitations vous êtes maintenant enregistré!')
+        return redirect(url_for('etablir_session'))
+    return render_template('enregistrement.html', title='Enregistrer',formulaire=formulaire)
+
+
 
 
 @app.route('/etablir_session',methods=['GET','POST'])
